549f5c36b7151d1384474c1994c023bd24f330a1
Kayla Martinez <kaylamartinez1201@gmil.com>
2023-10-22 19:27:42 -0400

    Todo1: background color changed added based on selectedAnswer

--- src/components/HeartDoodle/index.tsx
+++ src/components/HeartDoodle/index.tsx
@@ -1,13 +1,22 @@
+import { useContext } from 'react';
+import { AppContext } from '@context';
+import { Answers } from '@types';
 import Image from 'next/image';
 import heartDoodle from '@public/noun-heart-doodle-335497.svg';
 import styles from '@styles/HeartDoodle.module.scss';
 
 const HeartDoodle = () => {
+
+const { selectedAnswer } = useContext(AppContext)
+
+const doodleColor = () => (selectedAnswer === Answers.YES) ? 'red' : (selectedAnswer === Answers.NO) ? 'blue' : (selectedAnswer === Answers.MAYBE) ? 'purple' : ''
+
   return (
     <Image
       className={styles.heartDoodle}
       src={heartDoodle}
       alt="heartDoodle"
+      style={{backgroundColor: doodleColor()}}
     />
   );
 };
